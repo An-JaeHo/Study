3715d53556a0ec404fb8b5ce18d1de99eed51d0c
AnJaeHo <61310319+An-JaeHo@users.noreply.github.com>
2024-02-14 00:10:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.72 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        string[] ss = s.ToLower().Split(" ");
+            
+        for(int i =0; i<ss.Length;i++)
+        {
+            for(int j=0; j<ss[i].Length; j++)
+            {
+                if(j ==0)
+                {
+                    answer += ss[i][j].ToString().ToUpper();
+                }
+                else
+                {
+                    answer += ss[i][j];
+                }
+                
+            }
+            
+            
+            
+            if(i+1 != ss.Length)
+            {
+                 answer += " ";
+            }
+        }
+        
+        return answer;
+    }
+}
